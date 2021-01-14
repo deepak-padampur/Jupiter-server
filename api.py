a8dbbaf2f9fdbc343f655971bb653e1cf940f8c3
deepak-padampur <deepaksuna4290@gmail.com>
2021-01-14 15:44:10 +0530

    user creation route fixed

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 
 
-from flask import Flask
+from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
+import uuid
+from werkzeug.security import  generate_password_hash,check_password_hash
 import os
 
 
@@ -16,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+ os.path.join(basedir,'db.sql
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db=SQLAlchemy(app)
-db.create_all()
+
 
 class Restaurant(db.Model):
 	id=db.Column(db.Integer,primary_key=True)
@@ -25,6 +27,12 @@ class Restaurant(db.Model):
 	password=db.Column(db.String(80))
 	admin=db.Column(db.Boolean)
 
+class User(db.Model):
+	id=db.Column(db.Integer,primary_key=True)
+	public_id=db.Column(db.String(50),unique=True)
+	name=db.Column(db.String(50))
+	password=db.Column(db.String(80))
+
 class ProductCatalog(db.Model):
 	id=db.Column(db.Integer,primary_key=True)
 	title=db.Column(db.String(50))
@@ -32,6 +40,44 @@ class ProductCatalog(db.Model):
 	price=db.Column(db.Integer)
 	status=db.Column(db.Boolean)
 	restaurant_id=db.Column(db.Integer)
+
+db.create_all()
+
+
+@app.route('/product-catalog',methods=['POST'])
+def add_product():
+	return ''
+
+
+@app.route('/product-catalog',methods=['GET'])
+def get_all_product():
+	return ''
+
+@app.route('/product-catalog/<restaurant_id>',methods=['GET'])
+def get_product_byId():
+	return ''
+
+
+
+@app.route('/restaurant',methods=['GET'])
+def get_app_restaurant():
+	return ''
+
+@app.route('/user',methods=['POST'])
+def create_user():
+	data=request.get_json()
+
+	hashed_password=generate_password_hash(data['password'],method='sha256')
+
+	new_user= User(public_id=str(uuid.uuid4()),name=data['name'],password=hashed_password)
+	db.session.add(new_user)
+	db.session.commit()
+
+	return jsonify({'message':'new user created'})
+
+@app.route('/user/<user_id>',methods=['PUT'])
+def update_user():
+	return ''
 
 #Run server
 

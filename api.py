9c80008516815df009b7222872ecf68db78f9116
deepak-padampur <deepaksuna4290@gmail.com>
2021-01-14 16:50:50 +0530

    token created during login

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 
 
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,make_response
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import  generate_password_hash,check_password_hash
+import datetime
+import jwt
 import os
 
 
@@ -65,6 +67,8 @@ def get_product_byId():
 def get_app_restaurant():
 	return ''
 
+#create user
+
 @app.route('/user',methods=['POST'])
 def create_user():
 	data=request.get_json()
@@ -75,7 +79,9 @@ def create_user():
 	db.session.add(new_user)
 	db.session.commit()
 
-	return jsonify({'message':'new user created'})
+	return jsonify({'message':'new user created','status':'201'})
+
+#get all user list
 
 @app.route('/user',methods=['GET'])
 def get_all_user():
@@ -90,12 +96,14 @@ def get_all_user():
 		output.append(user_data)
 	return jsonify({'users':output})
 
+#get user by their public id number
+
 @app.route('/user/<public_id>',methods=['GET'])
 def get_user_byId(public_id):
 	user=User.query.filter_by(public_id=public_id).first()
 
 	if not user:
-		return jsonify({'message':'No user found'})
+		return jsonify({'message':'No user found','status':'404'})
 	user_data={}
 	user_data['public_id']=user.public_id
 	user_data['name']=user.name
@@ -103,11 +111,59 @@ def get_user_byId(public_id):
 
 	return jsonify({'user':user_data})
 
+#update user data
 
 
-@app.route('/user/<user_id>',methods=['PUT'])
-def update_user():
-	return ''
+@app.route('/user/<public_id>',methods=['PUT'])
+def update_user(public_id):
+	user=User.query.filter_by(public_id=public_id).first()
+
+	if not user:
+		return jsonify({'message':'No user found','status':'404'})
+
+	data=request.get_json()
+	user.name=data['name']
+	db.session.commit()
+
+
+	return jsonify({'message':'Updated details successfully'})
+
+#delete user data
+
+@app.route('/user/<public_id>',methods=['DELETE'])
+def delete_user(public_id):
+	user=User.query.filter_by(public_id=public_id).first()
+
+	if not user:
+		return jsonify({'message':'No user found'})
+
+
+	db.session.delete(user)
+	db.session.commit()
+
+	return jsonify({'message':'User has been deleted successfully'})
+
+
+#login resgitered users
+
+@app.route('/login')
+def login():
+	auth=request.authorization
+
+	if not auth or not auth.username or not auth.password:
+		return make_response('Could not verify',401,{'WWW-Authenticate':'Basic realm="Login required!"'})
+
+	user=User.query.filter_by(name=auth.username).first()
+
+	if not user:
+		return make_response('Could not verify',401,{'WWW-Authenticate':'Basic realm="Login required!"'})
+
+	if check_password_hash(user.password, auth.password):
+		token=jwt.encode({'public_id':user.public_id,'exp':datetime.datetime.utcnow()+datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
+		return jsonify({'token':token})
+
+	return make_response('Could not verify',401,{'WWW-Authenticate':'Basic realm="Login required!"'})
+    
 
 #Run server
 
